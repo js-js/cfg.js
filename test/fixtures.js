c2e50a6e624e951b2ab7c8bbce94392c592f4bd0
Fedor Indutny <fedor@indutny.com>
2016-05-18 15:01:43 -0400

    lib: save progress

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,63 +1,13 @@
-function strip(source) {
-  var lines = source.split(/\r\n|\r|\n/g);
+'use strict';
 
-  var out = lines.map(function(line) {
-    return line.replace(/^\s*/, '');
-  }).filter(function(line) {
-    return !!line;
-  });
+const esprima = require('esprima');
 
-  return out.join('\n');
+function fn2str(fn) {
+  return fn.toString().replace(/^\([^{]+{|}$/g, '');
 }
-exports.strip = strip;
+exports.fn2str = fn2str;
 
-function equalLines(actual, expected) {
-  if (actual === expected)
-    return;
-
-  var actualLines = actual.split('\n');
-  var expectedLines = expected.split('\n');
-  var width = 0;
-
-  expectedLines.unshift('    expected:');
-  actualLines.unshift('    actual:');
-  var total = Math.max(actualLines.length, expectedLines.length);
-
-  if (actualLines.length !== total) {
-    for (var i = actualLines.length; i < total; i++)
-      actualLines.push('');
-  } else {
-    for (var i = expectedLines.length; i < total; i++)
-      expectedLines.push('');
-  }
-
-  for (var i = 0; i < total; i++) {
-    width = Math.max(width, actualLines[i].length);
-    width = Math.max(width, expectedLines[i].length);
-  }
-
-  var out = '';
-  for (var i = 0; i < total; i++) {
-    var left = expectedLines[i];
-    var right = actualLines[i];
-
-    if (left !== right)
-      out += '\033[31m';
-    else
-      out += '\033[32m';
-
-    out += left;
-    for (var j = left.length; j < width; j++)
-      out += ' ';
-
-    out += '  |  ';
-    out += right;
-
-    out += '\033[0m';
-
-    out += '\n';
-  }
-
-  throw new Error('Output mismatch:\n\n' + out + '\n' + actual);
+function parse(fn) {
+  return esprima.parse(fn2str(fn), { loc: true });
 }
-exports.equalLines = equalLines;
+exports.parse = parse;
